5dece5c8765562fc327efb59f1a4c5eaf2fb65d7
AlisonTristao <AlisonTristao@hotmail.com>
2022-08-05 10:32:30 -0300

    Controle bluetooth para o carrinho papalegos

--- ControleBluetooth/app/src/main/java/com/example/controlebluetooth/Dispositivos.java
+++ ControleBluetooth/app/src/main/java/com/example/controlebluetooth/Dispositivos.java
@@ -1,6 +1,6 @@
 package com.example.controlebluetooth;
 
-//importa a lista (janelinha q aparece na tela)
+// importa a lista (janelinha q aparece na tela)
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -23,7 +23,7 @@ import java.util.Set;
 
 public class Dispositivos extends ListActivity {
 
-    //endereço mac e o bluetooth do celular
+    // endereço mac e o bluetooth do celular
     static String endMac = null;
     BluetoothAdapter adapBT = null;
 
@@ -32,23 +32,23 @@ public class Dispositivos extends ListActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //cria a lista (como uma janela)
+        // cria a lista (como uma janela)
         ArrayAdapter<String> disp = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1);
 
-        adapBT = BluetoothAdapter.getDefaultAdapter();//adaptador bluetooth
-        //pega a lista dos dispositivos pareados
+        adapBT = BluetoothAdapter.getDefaultAdapter();// adaptador bluetooth
+        // pega a lista dos dispositivos pareados
         Set<BluetoothDevice> dispPareados;
         dispPareados = adapBT.getBondedDevices();
 
-        //preenche a lista de dispositivos na lista
-        if(dispPareados.size() > 0) {//verifica se tem algum
-            for (BluetoothDevice dispo : dispPareados) {//para cada dispBT add na lista
-                //pega o nome e o endereço mac
+        // preenche a lista de dispositivos na lista
+        if(dispPareados.size() > 0) {// verifica se tem algum
+            for (BluetoothDevice dispo : dispPareados) {// para cada dispBT add na lista
+                // pega o nome e o endereço mac
                 String nomeDisp = dispo.getName();
                 String macDisp = dispo.getAddress();
 
-                //add na lista (\n = quebra de linha)
+                // add na lista (\n = quebra de linha)
                 disp.add(nomeDisp + "\n" + macDisp);
             }
         }else{
@@ -56,24 +56,24 @@ public class Dispositivos extends ListActivity {
                                                                     Toast.LENGTH_SHORT).show();
         }
 
-        //define pra ser visivel
+        // define pra ser visivel
         setListAdapter(disp);
     }
 
-    //quando clica em algum item da lista
+    // quando clica em algum item da lista
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        //pega os dados totais da area de texto clicada
+        // pega os dados totais da area de texto clicada
         String dados = ((TextView) v).getText().toString();
 
-        //endereço mac são os 17 primeiros caracteres
+        // endereço mac são os 17 primeiros caracteres
         String mac = dados.substring(dados.length() - 17);
 
-        //intent para retornar o endereco mac pra fazer coneão
+        // intent para retornar o endereco mac pra fazer coneão
         Intent retornaMac = new Intent();
-        retornaMac.putExtra(endMac, mac);//retorna o endereco mac com nome endMac
-        setResult(RESULT_OK, retornaMac);//retorna ok
-        finish();//fecha a lista
+        retornaMac.putExtra(endMac, mac);// retorna o endereco mac com nome endMac
+        setResult(RESULT_OK, retornaMac);// retorna ok
+        finish();// fecha a lista
     }
 }
--- ControleBluetooth/app/src/main/java/com/example/controlebluetooth/MainActivity.java
+++ ControleBluetooth/app/src/main/java/com/example/controlebluetooth/MainActivity.java
@@ -29,50 +29,50 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 
     private static MainActivity.conTh conTh;
-    //cria os componentes q serão usados (botoes e txt...)
+    // cria os componentes q serão usados (botoes e txt...)
     private Button btnConectar;
     private TextView txtDir;
     private TextView txtEsq;
     private SeekBar seekbarEsq;
     private SeekBar seekbarDir;
-    private int valores[] = new int[2];//guarda a velocidade de cada motor - esq 0 e dir 1
+    private int valores[] = new int[2];// guarda a velocidade de cada motor - esq 0 e dir 1
 
-    //variaveis para a conexão bluetoohth
-    BluetoothAdapter adapBT = null;//adaptador bluetooth
-    BluetoothDevice devBT = null; //dispositipo bluetooth
-    BluetoothSocket socBT = null; //entrada/canal bluetooth
-    public boolean con = false; //conectado ou nao
-    static String endMac = null; //endereço mac do dispositivo
+    // variaveis para a conexão bluetoohth
+    BluetoothAdapter adapBT = null;// adaptador bluetooth
+    BluetoothDevice devBT = null; // dispositipo bluetooth
+    BluetoothSocket socBT = null; // entrada/canal bluetooth
+    public boolean con = false; // conectado ou nao
+    static String endMac = null; // endereço mac do dispositivo
     /*maracutaia pra comunicação bt (todas os dispositivos devem ter esse UUID)
     cada dispositivo tem uma porta UUID diferente, então vai dar erro se tentar conectar com algo
     diferente (computador, tablet...)*/
     UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
     private Handler handler; // handler that gets info from Bluetooth service
 
-    //(cada request tem um int para sua identificação)
-    public static final int REQ_BT = 1; //requisição da ativação bt
-    public static final int REQ_CON = 2; //requisição para conexão bt
+    // (cada request tem um int para sua identificação)
+    public static final int REQ_BT = 1; // requisição da ativação bt
+    public static final int REQ_CON = 2; // requisição para conexão bt
 
     public void iniciarComponentes(){
-        //define os componentes que criamos como os elementos do xml
+        // define os componentes que criamos como os elementos do xml
         btnConectar = findViewById(R.id.btnConectar);
-        txtEsq = findViewById(R.id.txtDir);//(botei invertido kkkkkk)
+        txtEsq = findViewById(R.id.txtDir);// (botei invertido kkkkkk)
         txtDir = findViewById(R.id.txtEsq);
         seekbarEsq = findViewById(R.id.seekBar);
         seekbarDir = findViewById(R.id.seekBar2);
 
-        //full screen
+        // full screen
         hideSystemBars();
     }
 
-    //esconde as barras de navegação e barra de status
+    // esconde as barras de navegação e barra de status
     protected void hideSystemBars() {
         //deixa app em full screen
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY//define q mesmo se encostar na tela continua hide
-        | View.SYSTEM_UI_FLAG_FULLSCREEN //esconde status bar
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION//esconde statusbar
+          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY // define q mesmo se encostar na tela continua hide
+        | View.SYSTEM_UI_FLAG_FULLSCREEN // esconde status bar
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // esconde statusbar
         );
     }
 
@@ -82,59 +82,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //inicia os componentes
+        // inicia os componentes
         iniciarComponentes();
 
         // -------- inicia o bluetooth -------- //
 
         adapBT = BluetoothAdapter.getDefaultAdapter();
-        //verifica se o dispositivo possui bluetooth
+        // verifica se o dispositivo possui bluetooth
         if (adapBT == null) {
-            //dispositivo não suporta bluetooth
+            // dispositivo não suporta bluetooth
             Toast.makeText(this, "Seu dispositivo não possui Bluetooth!",
                     Toast.LENGTH_SHORT).show();
-            //é preciso verifica pois se tentar ligar o bt sem ter bt crasha o app
-        } else if (!adapBT.isEnabled()) { //verifica se o bluetooth está desligado
-            //pede pra ligar o bluetooth
-            //(aqui da um erro q n sei o pq, eu suprimi e funcionou ok)
+            // é preciso verifica pois se tentar ligar o bt sem ter bt crasha o app
+        } else if (!adapBT.isEnabled()) { // verifica se o bluetooth está desligado
+            // pede pra ligar o bluetooth
+            // (aqui da um erro q n sei o pq, eu suprimi e funcionou ok)
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQ_BT);
 
-            //esconde o hide
+            // esconde o hide
             hideSystemBars();
         }
 
         // -------- codigos dos botoes -------- //
 
-        //abre a lista de dispositivos pareados ou desconecta
+        // abre a lista de dispositivos pareados ou desconecta
         btnConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //se não estiver conectado abre a lista de dispositivos
-                if (con) {//se estiver conectado
-                    try{//tenta desconectar
+                // se não estiver conectado abre a lista de dispositivos
+                if (con) {// se estiver conectado
+                    try{// tenta desconectar
                         socBT.close();
                         Toast.makeText(MainActivity.this, "Desconectado!",
                                 Toast.LENGTH_SHORT).show();
-                        //define como desconectado
+                        // define como desconectado
                         con = false;
                         btnConectar.setText("Conectar");
                     } catch (IOException er) {//mensagem de erro
                         Toast.makeText(MainActivity.this, "Erro ao desconectar!",
                                 Toast.LENGTH_SHORT).show();
                     }
-                } else {//senão abre uma lista com dispositivos pareados
+                } else {// senão abre uma lista com dispositivos pareados
                     Intent abreList = new Intent(MainActivity.this, Dispositivos.class);
                     startActivityForResult(abreList, REQ_CON);
                 }
             }
         });
 
-        //atualiza o valor de valores quando muda a posição do seekbar
+        // atualiza o valor de valores quando muda a posição do seekbar
         seekbarEsq.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                //atualiza o valor no vetor
+                // atualiza o valor no vetor
                 valores[0] = i;
 
                 //altera o txt
@@ -143,12 +143,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                //quando clica na seekbar
-                //(não faz nada)
+                // quando clica na seekbar
+                // (não faz nada)
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //quando solta a seekbar
+                // quando solta a seekbar
                 seekbarEsq.setProgress(0);//volta pra zero
             }
         });
@@ -156,22 +156,22 @@ public class MainActivity extends AppCompatActivity {
         seekbarDir.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                //atualiza o valor no vetor
+                // atualiza o valor no vetor
                 valores[1] = i;
 
-                //altera o txt
+                // altera o txt
                 txtDir.setText(Integer.toString(i));
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                //quando clica na seekbar
-                //(não faz nada)
+                // quando clica na seekbar
+                // (não faz nada)
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                //quando solta a seekbar
+                // quando solta a seekbar
                 seekbarDir.setProgress(0);//volta pra zero
             }
         });
@@ -181,70 +181,70 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {//verifica qual a requisição feita
-            case REQ_BT://caso for uma requisição para ligar o bt
-                if (resultCode == Activity.RESULT_OK) {//se clicou ok
-                } else {//se não deixou ligar o bt
+        switch (requestCode) {// verifica qual a requisição feita
+            case REQ_BT:// caso for uma requisição para ligar o bt
+                if (resultCode == Activity.RESULT_OK) {// se clicou ok
+                } else {// se não deixou ligar o bt
                     Toast.makeText(this, "Erro ao ligar o Bluetooth!",
                                                                     Toast.LENGTH_SHORT).show();
                 }
                 break;
 
-            case REQ_CON://caso seja requisição para conectar em um dispositivo
-                if (resultCode == Activity.RESULT_OK) {//temos o endereço mac que retorna da lista
-                    //salva o endereço mac q retornou
+            case REQ_CON:// caso seja requisição para conectar em um dispositivo
+                if (resultCode == Activity.RESULT_OK) {// temos o endereço mac que retorna da lista
+                    // salva o endereço mac q retornou
                     endMac = data.getExtras().getString(Dispositivos.endMac);
 
-                    //buscamos o dispositivo bluetooth pelo seu endereço mac
+                    // buscamos o dispositivo bluetooth pelo seu endereço mac
                     devBT = adapBT.getRemoteDevice(endMac);
 
-                    //criamos um canal de comunicação e começamos a enivar os dados
+                    // criamos um canal de comunicação e começamos a enivar os dados
                     try {
-                        //criamos um canal de comunicação com UUID
+                        // criamos um canal de comunicação com UUID
                         socBT = devBT.createInsecureRfcommSocketToServiceRecord(uuid);
 
-                        //conecta com o dispostivo
+                        // conecta com o dispostivo
                         socBT.connect();
 
-                        //define os fluxos de entrada e saida de dados
+                        // define os fluxos de entrada e saida de dados
                         conTh = new conTh(socBT);
                         conTh.start();
 
-                        //mensagem
+                        // mensagem
                         Toast.makeText(this, "Conectado com: " + endMac,
                                                                     Toast.LENGTH_SHORT).show();
 
-                        //define como conectado
+                        // define como conectado
                         con = true;
                         btnConectar.setText("Desconectar");
 
-                        //começa enviar os dados bluetooth
+                        // começa enviar os dados bluetooth
                         new th().start();
 
-                    }catch (IOException er){//mensagem de erro
+                    }catch (IOException er){// mensagem de erro
                         Toast.makeText(this, "Erro ao criar comunicação bluetooth!",
                                                                     Toast.LENGTH_SHORT).show();
 
-                        //define como desconectado
+                        // define como desconectado
                         con = false;
                         btnConectar.setText("Conectar");
 
                     }
-                }else{//nao conseguiu obeter o endereço mac
+                }else{// nao conseguiu obeter o endereço mac
                     Toast.makeText(this, "Erro ao obeter endereço Mac!",
                                                                     Toast.LENGTH_SHORT).show();
                 }
 
-                //full screen
+                // full screen
                 hideSystemBars();
                 break;
         }
     }
 
-    //classe para enviar e receber dados pela porta/canal socket
+    // classe para enviar e receber dados pela porta/canal socket
     private class conTh extends Thread {
-        private final InputStream mmInStream;//saida
-        private final OutputStream mmOutStream;//entrada
+        private final InputStream mmInStream;// saida
+        private final OutputStream mmOutStream;// entrada
 
         public conTh(BluetoothSocket socket) {
             InputStream tmpIn = null;
@@ -267,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
             mmOutStream = tmpOut;
         }
 
-        //isso aqui é diferente (tem forma melhor de obter os dados)
+        // isso aqui é diferente (tem forma melhor de obter os dados)
         /*public void run() {
             mmBuffer = new byte[1024];
             int numBytes; // bytes returned from read()
@@ -290,10 +290,10 @@ public class MainActivity extends AppCompatActivity {
 
         // enviar dados
         public void enviar(String texto) {
-            //converte String pra um vetor de bytes
+            // converte String pra um vetor de bytes
             byte[] bytes = texto.getBytes();
             try{
-                //fluxo de saida (envia os bytes)
+                // fluxo de saida (envia os bytes)
                 mmOutStream.write(bytes);
             } catch (IOException e) {
                 Toast.makeText(MainActivity.this, "Erro ao enviar dados!",
@@ -302,13 +302,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class th extends Thread{//função q vai executar em segundo plano
+    public class th extends Thread{// função q vai executar em segundo plano
         public void run() {
-            while (con == true){//enquanto a conexão estiver ativa
+            while (con == true){// enquanto a conexão estiver ativa
                 //manda os valores
                 MainActivity.conTh.enviar("[" + valores[0] + "," + valores[1] + "]");
                 try {
-                    //espera 100 milis segundos
+                    // espera 100 milis segundos
                     sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
